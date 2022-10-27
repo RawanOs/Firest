647bfdefcfd2d97ea97c936857c4f19845e36e15
Rawan <roalsufyani22@twq.idserve.net>
2022-10-27 09:56:42 +0300

    Initial Commit

---
+++ me/meApp.swift
@@ -0,0 +1,17 @@
+//
+//  meApp.swift
+//  me
+//
+//  Created by Rawan on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
